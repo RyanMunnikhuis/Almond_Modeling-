4cd250788431e8acd0d21d5ddfa97581a556fa15
RyanMunnikhuis <Rmunnikh@gmail.com>
2022-04-18 18:09:19 -0700

    updated function

--- almond_price_fluctuation.R
+++ almond_price_fluctuation.R
@@ -1,6 +1,6 @@
-almond_price_flucutation <- function(yield_anomaly, mean_almond_price) {
-  almond_price <-( mean_almond_price - (yield_anomaly *.01))
+almond_price_flucutation <- function(yield_anomaly, mean_almond_price, num_of_acres = 1, avg_pounds = 2000) {
+  almond_price <-(mean_almond_price - (mean_almond_price * yield_anomaly * .001))
     return(almond_price)
   }
 
-almond_price_flucutation()
+
