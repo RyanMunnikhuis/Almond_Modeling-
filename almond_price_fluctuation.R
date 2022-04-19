46d24650cf1eadaaccf9b2e323549b44757ae7cf
Daniel Kerstan <danielkerstan@bren.ucsb.edu>
2022-04-18 18:33:56 -0700

    updated model

--- almond_price_fluctuation.R
+++ almond_price_fluctuation.R
@@ -1,6 +1,22 @@
-almond_price_flucutation <- function(yield_anomaly, mean_almond_price, num_of_acres = 1, avg_pounds = 2000) {
-  almond_price <-(mean_almond_price - (mean_almond_price * yield_anomaly * .001))
-    return(almond_price)
+almond_price_flucutation <- function(yield_anomaly, mean_almond_price, num_of_acres = 1, base_yield = 1, cost = 2000) {
+  
+  price <- mean_almond_price - ((mean_almond_price * .10) * yield_anomaly)
+  
+  total_anom <- num_of_acres * yield_anomaly
+  
+  total_base_yield <- num_of_acres * base_yield
+  
+  total_yield <- total_base_yield + total_anom
+  
+  money <- total_yield * mean_almond_price
+  
+  total_cost <- cost * num_of_acres
+  
+  profit <- money - total_cost
+  
+  
+  return(profit)
+  
   }
 
 
